f27189b874aa7f7805691c5c40de3162b614207d
SABRINA SOUIKEY <sabrina.souikey@itstechtalentfactory.it>
2025-01-30 14:42:15 +0100

    tubouser

---
+++ src/test/java/com/ttf/Users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttf.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
