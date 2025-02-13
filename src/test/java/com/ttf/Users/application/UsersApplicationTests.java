5f4792a1c261bdd28b5b800bdfe66bbace7791f9
Alessandro Franceschi <al@example42.com>
2025-02-13 16:43:20 +0100

    new version

--- src/test/java/com/ttf/Users/application/UsersApplicationTests.java
+++ src/test/java/com/ttf/Users/application/UsersApplicationTests.java
@@ -3,8 +3,8 @@ package com.ttf.Users.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
-@SpringBootTest(Classes=UsersApplication.class)
 class UsersApplicationTests {
 
 	@Test
