aa6d185a9cf027c4f7e78187c1f346ab9d2fab2e
SABRINA SOUIKEY <sabrina.souikey@itstechtalentfactory.it>
2025-02-13 15:40:44 +0100

    test1

--- src/test/Application/UsersApplicationTests.java
+++ src/test/Application/UsersApplicationTests.java
@@ -1,9 +1,10 @@
-package com.ttf.Users;
+package com.ttf.Users.Application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootTest(Classes=UsersApplication.class)
 class UsersApplicationTests {
 
 	@Test
